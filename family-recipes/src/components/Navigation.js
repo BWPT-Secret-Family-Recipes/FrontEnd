14d3c1368ed7669debf47902855e9754e50475fb
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-12-21 23:11:19 -0500

    created components to test heroku app for creating a user

---
+++ family-recipes/src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Navbar,Nav,NavLink} from 'reactstrap'
+
+const Navigation = () => {
+    const logout = () => {
+        localStorage.removeItem('token')
+    }
+    return (
+        <Navbar color='dark'>
+            <Nav>
+                <NavLink href="/">Home</NavLink>
+                <NavLink href="/sign-in">Sign-In</NavLink>
+                <NavLink href="/login" onClick={logout}>Log-Out</NavLink>
+                <NavLink href="/my-profile">my-profile</NavLink>
+            </Nav>
+        </Navbar>
+    )
+}
+
+export default Navigation
---
+++ family-recipes/src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+    return (
+        <div>
+            Signing UP
+        </div>
+    )
+}
+
+export default SignUp;
