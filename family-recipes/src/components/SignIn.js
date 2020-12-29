1887664e2b8448ccb42b3fefc8f01270bd81ece6
Elijah <elijahoneal@yahoo.com>
2020-12-28 20:13:19 -0500

    Yup Validation + Display error message

--- family-recipes/src/components/SignIn.js
+++ family-recipes/src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import * as yup from 'yup';
 
@@ -16,34 +16,40 @@ const formSchema = yup.object().shape({
 
 
 const SignIn = () => {
-    const formData = { name: "", password: ""}
-    const [ user , setUser] = useState(formData);
-    const [err , setErr] = useState(formData);
+    // const formData = { name: "", password: ""}
+    const [ user , setUser] = useState({ name: "", password: ""});
+    const [errors , setErrors] = useState({name: "", password: ""});
+    const [disabled , setDisabled] = useState(true);
+    
    
     const setFormErrors = (name , value) => {
         yup.reach(formSchema , name).validate(value)
-            .then( () => setErr({ ...err , [name]: "" }))
-            .catch( e => setErr( {...err , [name]: e.err } ))
+            .then( () => setErrors({ ...errors , [name]: "" }))
+            .catch( err => setErrors( {...errors , [name]: err.errors[0] } ))
     }
     
     const handleChange = event => {
         const { name, value} = event.target;
+        
         setFormErrors(name , value);
-        setUser({...user , [name] : value});
+        setUser({...user , [name]: value});
         
     }
 
+      useEffect( () =>{
+        formSchema.isValid(user)
+        .then(valid => {setDisabled(!valid) })
+    }, [user] )
 
     const submitForm  = (event) => {
         event.preventDefault();
          axios.post("https://reqres.in/api/users" , user)
             .then( res => {
                 console.log("success" , res.data);
-                setUser(formData);
+                setUser({ name: "", password: ""});
             })
             
             .catch(err => console.log(err, "submission failed"))
-            console.log(user);
     }
    
 
@@ -51,6 +57,7 @@ const SignIn = () => {
         <div>
             <h1>Signing in</h1>
             <form onSubmit = {submitForm}>
+                <p>{errors.name}</p>
                 <label htmlFor = "username">Username</label>
                     <input
                     id="name"
@@ -59,7 +66,7 @@ const SignIn = () => {
                     placeholder = "Enter Username"
                     onChange = {handleChange}
                     />
-
+                <p>{errors.password}</p>
                 <label htmlFor = "password">Password</label>
                     <input
                     id="password"
@@ -67,7 +74,7 @@ const SignIn = () => {
                     type = "password"
                     onChange = {handleChange}
                     />
-                <button type = "submit">Sign In</button>
+                <button disabled = {disabled} type = "submit">Sign In</button>
             </form>
         </div>
     )
