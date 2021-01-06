78067ae1ccb94aed262fbd72a6191c536f0a1cfb
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-06 00:58:00 -0500

    nav, sign up text change

--- family-recipes/src/components/Navigation.js
+++ family-recipes/src/components/Navigation.js
@@ -16,7 +16,7 @@ const Navigation = () => {
         <Navbar color='dark'>
             <Img src = {logo} alt = "secret family recipes logo"></Img>
             <Nav>
-                <NavLink style={{color:'white'}}href="/home-page">Home</NavLink>
+                <NavLink style={{color:'white'}}href="/">Home</NavLink>
                 <NavLink style={{color:'white'}} href="/sign-in">Sign-In</NavLink>
                 <NavLink style={{color:'white'}} href="/register">Register</NavLink>
                 <NavLink style={{color:'white'}} href="/login" onClick={logout}>Log-Out</NavLink>
